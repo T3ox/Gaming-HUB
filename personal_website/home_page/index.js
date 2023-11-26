37fc32afcc277c77e5380583f361dfcf9df19162
Matteo <you@example.com>
2023-11-26 19:49:01 +0100

    miglioramento css e corretto impaginazione della home page

---
+++ personal_website/home_page/index.js
@@ -0,0 +1,13 @@
+const menuDescription = document.getElementById('menu-description');
+const imgMenu = document.querySelector('img-container');
+const lanDescription = document.getElementById('menu-description');
+const imgLan = document.querySelector('img-container');
+
+menuDescription.addEventListener('click', function() {
+    window.open("/personal_website/inner pages/Menu/index.html", "_self");
+});
+imgMenu.addEventListener('click', function() {
+    window.open("/personal_website/inner pages/Menu/index.html", "_self");
+});
+
+
