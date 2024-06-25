1972d4a0e5f21e6b11005df3beadc001687b91fb
T3ox <matteo.t3ox@gmail.com>
2024-06-25 19:05:48 +0200

    hamburger detail flip card

--- gaming-hub/src/components/molecules/Hamburger/Hamburger.tsx
+++ gaming-hub/src/components/molecules/Hamburger/Hamburger.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Props from "../../../utils/type";
 import "./styles.scss";
 const portal = require("../../../images/portal.png") as string;
-//const panino = require("../../../images/Buddy_nobg.png") as string;
+const portalReverse = require("../../../images/portal_back.png") as string;
 
 const HamburgerComponent: React.FC<Props> = ({ name, description, img }) => {
     const [isHovered, setIsHovered] = useState(false);
@@ -11,32 +11,32 @@ const HamburgerComponent: React.FC<Props> = ({ name, description, img }) => {
         setIsHovered(!isHovered);
     };
 
-    /*
-    className={` panino-shower ${
-                    isHovered ? "ingredients-toggled" : ""
-                }`}
-    */
-
     return (
         <div className="panino-container d-flex flex-column align-items-center">
-            {/*<div className={`panino-shower`}>
-                <img src={portal} alt="" id="portal" />
-                <img
-                    src={img}
-                    alt=""
-                    id="hamburger"
-                    className={`${isHovered ? "ingredients-toggled" : ""}`}
-                    onMouseEnter={toggleIngredient}
-                    onMouseLeave={toggleIngredient}
-                />
-                <p
-                    id="ingredients"
-                    dangerouslySetInnerHTML={{ __html: description }}
-                />
-            </div>*/}
-
             <div className="panino-shower">
-                
+                <div className="panino-inner">
+                    <div className="card-front">
+                        <img src={portal} alt="" id="portal" />
+                        <img
+                            src={img}
+                            alt=""
+                            id="hamburger"
+                            className={`${
+                                isHovered ? "ingredients-toggled" : ""
+                            }`}
+                            onMouseEnter={toggleIngredient}
+                            onMouseLeave={toggleIngredient}
+                        />
+                    </div>
+
+                    <div className="card-back">
+                        <img src={portalReverse} alt="" id="portal" />
+                        <p
+                            id="ingredients"
+                            dangerouslySetInnerHTML={{ __html: description }}
+                        />{" "}
+                    </div>
+                </div>
             </div>
             <span>{name}</span>
         </div>
