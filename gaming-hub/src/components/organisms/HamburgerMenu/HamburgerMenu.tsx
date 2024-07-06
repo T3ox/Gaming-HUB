16247076a2df5c2bbee1e856a885b4f6d46b38c6
T3ox <matteo.t3ox@gmail.com>
2024-07-06 16:29:48 +0200

    added loadings

--- gaming-hub/src/components/organisms/HamburgerMenu/HamburgerMenu.tsx
+++ gaming-hub/src/components/organisms/HamburgerMenu/HamburgerMenu.tsx
@@ -1,14 +1,29 @@
+import { useEffect, useState } from "react";
 import { menu } from "../../../utils/LocalDB/menuDatas";
-import HamburgerComponent from "../../molecules/Hamburger/Hamburger";
+import HamburgerComponent from "../../molecules/HamburgerComponent/HamburgerComponent";
 import "./styles.scss";
 
 const HamburgerMenu = () => {
+    const [loadedImages, setLoadedImages] = useState(0);
+
+    useEffect(() => {
+        menu.forEach((hamburger) => {
+            const img = new Image();
+            img.src = hamburger.img;
+            img.onload = () => {
+                setLoadedImages((prevCount) => {
+                    return prevCount + 1;
+                });
+            };
+        });
+    }, []);
+
     return (
-        <div className="container menu-container mt-5">
+        <div className="container menu-container mt-5 fade-in">
             <div className="row">
                 {menu.map((hamburger, index) => (
                     <div
-                        className=" col-6 col-md-4 col-xl-3 hamburger-container"
+                        className="col-6 col-md-4 col-xl-3 hamburger-container"
                         key={index}
                     >
                         <HamburgerComponent
--- gaming-hub/src/components/pages/Menu.tsx
+++ gaming-hub/src/components/pages/Menu.tsx
@@ -1,15 +1,39 @@
+import { useEffect, useState } from "react";
 import Dropdown from "../molecules/Dropdown/Dropdown";
 import HamburgerMenu from "../organisms/HamburgerMenu/HamburgerMenu";
 import NewestOffer from "../organisms/NewestOffer/NewestOffer";
 import "./styles.scss";
+
 const Menu = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <>
-            <NewestOffer />
-            <div className="body">
-                <HamburgerMenu />
-                <Dropdown />
-            </div>
+            {isLoading && (
+                <div className="loading-gif d-flex justify-content-center align-items-center">
+                    <img
+                        src={require("../../images/hambugerBuilding.gif")}
+                        alt="loading..."
+                    />
+                </div>
+            )}
+            {!isLoading && (
+                <div className="fade-in">
+                    <NewestOffer />
+                    <div className={`body`}>
+                        <HamburgerMenu />
+                        <Dropdown />
+                    </div>
+                </div>
+            )}
         </>
     );
 };
