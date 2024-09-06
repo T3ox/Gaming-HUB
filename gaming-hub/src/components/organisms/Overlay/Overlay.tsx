8ba374e8328d749056e43167b4e07e8c99e1d93c
unknown <matteo.t3ox@gmail.com>
2024-09-06 15:10:31 +0200

    overlay section bug resolved

--- gaming-hub/src/components/organisms/Overlay/Overlay.tsx
+++ gaming-hub/src/components/organisms/Overlay/Overlay.tsx
@@ -1,12 +1,12 @@
 import OverlayButton from "../../atoms/OverlayButton/OverlayButton";
 import "./styles.scss";
 const whatsappUrl = `https://wa.me/+393519735045`;
-
+const backgroundImage = require("../../../images/hub_menu.jpg");
 const Overlay = () => {
     return (
-        <div className="background-overlay text-white ">
+        <div className="background-overlay text-white d-flex justify-content-center align-items-center">
             <div className="overlay-column container">
-                <div className="row">
+                <div className="row" >
                     <div className="col-lg-6 d-flex flex-column overlay-content pb-4 pb-lg-0">
                         <h2 className="p-1">Prova le nostre specialità</h2>
                         <p className="p-1">
@@ -21,6 +21,7 @@ const Overlay = () => {
                     </div>
                 </div>
             </div>
+            <img className="overlay-image" src={backgroundImage} />
         </div>
     );
 };
