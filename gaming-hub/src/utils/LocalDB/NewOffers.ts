18b981965f9feb41f24715c20005a78950ace685
T3ox <matteo.t3ox@gmail.com>
2025-11-08 15:00:44 +0100

    add scary burger

--- gaming-hub/src/utils/LocalDB/NewOffers.ts
+++ gaming-hub/src/utils/LocalDB/NewOffers.ts
@@ -2,6 +2,17 @@ import Hamburger from "../Props";
 
 export const newOffers: Hamburger[] = [
     {
+        name: "Scary Burger",
+        description:
+            "Carne bovina piemontese, salsa al curry, doppio Cheddar, cipolla caramellata, pancetta croccante, insalata gentile, pomodoro ramato.",
+        prices: {
+            "100g": "12,00€",
+            "200g": "13,50€",
+            "300g": "16,00€",
+        },
+        img: require("../../images/Offers/scaryBurger.png") as string,
+    },
+    {
         name: "Season 6",
         description:
             "Carne bovina Piemontese, pesto di rucola e noci, Brie bretone, speck Alto Adige IGP, salsa dolcepiccante ai fichi, crumble di noci",
