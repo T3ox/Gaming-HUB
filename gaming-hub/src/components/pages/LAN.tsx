198317912540048a1e6d67b5f31215a4a210e335
T3ox <matteo.t3ox@gmail.com>
2026-02-08 11:09:52 +0100

    remove lan page

--- gaming-hub/src/components/pages/LAN.tsx
+++ gaming-hub/src/components/pages/LAN.tsx
@@ -1,28 +1,15 @@
-import { useRef } from "react";
 import DynamicPage from "../../utils/SEO/DynamicPage";
-import LanTitle from "../atoms/LanTitle/LanTitle";
-import Header from "../organisms/Header/Header";
-import PCShowcase from "../organisms/PCShowcase/PCShowcase";
-import PricingOverlay from "../organisms/PricingOverlay/PricingOverlay";
 import "./styles.scss";
-const bannerURL: string = require("../../images/esterno.jpg");
 
 const LAN = () => {
-    const targetRef = useRef<HTMLDivElement>(null);
-
     return (
         <>
             <DynamicPage pageName="Sala LAN" />
 
             <div className="header-lan">
-                <Header
-                    height="60svh"
-                    heightLg="60svh"
-                    image={bannerURL}
-                    content={<LanTitle targetRef={targetRef} />}
-                />
-                <PCShowcase ref={targetRef} />
-                <PricingOverlay />
+                <div className="wip d-flex justify-content-center align-items-center">
+                    <h2>Novità in arrivo</h2>
+                </div>
             </div>
         </>
     );
